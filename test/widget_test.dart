af09833b9b1389580dc6faec118ca4d491750563
David Senk <david@senk.me>
2024-09-04 18:11:10 -0400

    split into multiple files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:ything_radio/main.dart';
+import 'package:ything_radio/YthingRadio.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const YthingRadio());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
