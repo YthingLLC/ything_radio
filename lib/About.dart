db079c4d4ef058d5451425951755421204ab3380
David Senk <david@senk.me>
2024-09-04 19:42:08 -0400

    changed placeholders to text

--- lib/About.dart
+++ lib/About.dart
@@ -5,6 +5,8 @@ class About extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Text("About Page"),
+    );
   }
 }
--- lib/Links.dart
+++ lib/Links.dart
@@ -5,6 +5,8 @@ class Links extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Text("Links Page"),
+    );
   }
 }
